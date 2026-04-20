d9361c9969bcd3c68705f315098e256bbb6f8755
Smruti Ranjan Sahoo <s.r.sahoo370@gmail.com>
2026-04-20 10:53:13 +0530

    Refactor app structure by implementing tab navigation and new screens for meals and adding meals

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,27 @@
-import { Header, HeaderTitle } from "@react-navigation/elements";
-import { Stack } from "expo-router";
-import { StackScreen } from "react-native-screens";
+// import { colors } from '@/styles/global';
+// import { Stack } from 'expo-router';
+
+// export default function RootLayout() {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerStyle: { backgroundColor: colors.header },
+//         headerTintColor: '#fff',
+//       }}
+//     >
+//       <Stack.Screen name='index' options={{ headerShown: false }} />
+//       <Stack.Screen options={{ title: 'Meals' }} name='meals' />
+//       <Stack.Screen options={{ title: 'Add Meal' }} name='add-meal' />
+//     </Stack>
+//   );
+// }
+
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack 
-    screenOptions={{
-      // headerTitle:"Raja bhai",
-      headerShown:false
-    }}>
-      <Stack.Screen name="index"/>
-      <Stack.Screen name="meals"/>
-
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='(tabs)' />
     </Stack>
+  );
 }
